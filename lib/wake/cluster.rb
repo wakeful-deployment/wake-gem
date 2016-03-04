a376c672c95501ce4252fda997b04ee039728667
myobie <me@nathanherald.com>
2016-03-04 14:41:05 +0100

    Split the idea of Clusters and Cluster

--- lib/wake/cluster.rb
+++ lib/wake/cluster.rb
@@ -1,86 +1,31 @@
 require 'forwardable'
-require 'fileutils'
-require 'wake/string_utils'
-require 'config'
-
-path = File.expand_path(File.join(CONFIG_DIR, "clusters"))
-
-unless File.exists?(path)
-  FileUtils.mkdir_p(File.dirname(path))
-  File.open(path, mode: "w", universal_newline: true) do |f|
-    f << "{}"
-  end
-end
-
-CLUSTERS = JSONFile.new(path)
 
 class Cluster
   extend Forwardable
 
-  NoDefaultClusterSet = Class.new(StandardError)
+  attr_reader :name, :json_file
 
-  def self.default
-    name = Config.get("default_cluster")
-
-    if name
-      get(name)
-    else
-      fail NoDefaultClusterSet
-    end
-  end
-
-  def self.list
-    CLUSTERS.to_hash
-  end
-
-  def self.get(name)
-    if clusters.key?(name)
-      new(name)
-    end
-  end
-
-  def self.reload
-    clusters.reload
-  end
-
-  attr_reader :name
-
-  def initialize(name)
+  def initialize(name, json_file)
     @name = name
-  end
-
-  def full_key(key)
-    "#{name}.#{key}"
+    @json_file = json_file
   end
 
   def inspect
     "#<#{self.class.name} {#{name.inspect}}>"
   end
 
-  def [](key)
-    self.class.clusters[full_key(key)]
-  end
-
-  def require(key)
-    self.class.clusters.require(full_key(key))
-  end
-
-  def update(key, value)
-    self.class.clusters.update(full_key(key), value)
-  end
-
-  def to_hash
-    self.class.clusters[name].merge("name" => name)
-  end
-
-  def reload
-    self.class.reload
-    self
-  end
-
+  delegate [:key?, :[], :[]=, :require, :update, :delete, :reload, :empty?, :to_hash] => :json_file
 
   def iaas
     self["iaas"]
+  end
+
+  def datacenter
+    self["datacenter"]
+  end
+
+  def orchestrator
+    self["orchestrator"]
   end
 
   def dns_zone
@@ -89,9 +34,5 @@ class Cluster
 
   def collaborators
     self["collaborators"] || []
-  end
-
-  def delete
-    self.class.clusters.delete(name)
   end
 end
---
+++ lib/wake/clusters.rb
@@ -0,0 +1,53 @@
+require 'singleton'
+require 'wake/cluster'
+require 'wake/config'
+require 'wake-utils/json_file'
+
+class Clusters
+  include Singleton
+
+  def initialize
+    @path = File.expand_path File.join(Config.dir, "clusters")
+
+    unless File.exists? @path
+      FileUtils.mkdir_p File.dirname(path)
+    end
+  end
+
+  def file_path(name)
+    File.join @path, "#{name}.json"
+  end
+
+  def get(name)
+    load file_path(name)
+  end
+
+  def load(name:, path)
+    if File.exists?(path)
+      name ||= File.basename(path).split(".").first
+      json_file = JSONFile.new path
+      Cluster.new name, json_file
+    end
+  end
+
+  def create(name)
+    path = file_path name
+
+    unless File.exists? path
+      File.open(path, mode: "w", universal_newline: true) do |f|
+        f << "{}"
+      end
+    end
+  end
+
+  def default
+    get Config.get("default_cluster")
+  end
+
+  def all
+    path = file_path "**/*"
+    Dir[path].each do |file|
+      load file
+    end
+  end
+end
